1b5ddb11a2d9c424e949ae873359ee52d2c25cf7
chenzong24635 <chenzong24635@163.com>
2020-06-10 09:55:15 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './app'
 
-// if (module.hot) { module.hot.accept(); }
-
 new Vue({
   render: h => h(App)
 }).$mount('#app')
--- src/print.js
+++ src/print.js
@@ -1,3 +1,3 @@
 export default function printMe() {
-    console.log('5555')
+    console.log('43')
     }
